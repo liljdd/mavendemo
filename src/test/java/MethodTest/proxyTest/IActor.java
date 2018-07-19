f8d4e22582f3e31576cd211acfc20d8a191213c4
lilj <lilj@mchina.cn>
2018-07-19 09:56:05 +0800

    动态代理

---
+++ src/test/java/MethodTest/proxyTest/IActor.java
@@ -0,0 +1,19 @@
+package MethodTest.proxyTest;
+
+/**
+ * @author lilj
+ * @date 18/07/19
+ */
+public interface IActor {
+    /**
+     * 基本演出
+     * @param money
+     */
+    public void basicAct(float money);
+    /**
+     * 危险演出
+     * @param money
+     */
+    public void dangerAct(float money);
+
+}
